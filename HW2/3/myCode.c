ddc53ac7affe215ed65cd633e5f3206a7eb9f053
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-12 22:07:41 +0000

    HW2

--- HW2/3/myCode.c
+++ HW2/3/myCode.c
@@ -7,12 +7,9 @@ float number;
 char character;
 printf("Enter a decimal number:");
 scanf("%f", &number);
-printf("%d\n", number);
 printf("Enter an integer:");
 scanf("%d", &integer);
-printf("%f\n", integer);
 printf("Enter a character:");
 scanf("%s", character);
-printf("%d\n", character);
-
+printf(" Decimal: %d, Integer: %f, Character: %d\n", number, integer, character);
 }
